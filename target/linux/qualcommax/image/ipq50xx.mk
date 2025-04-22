5a0cc0e88db15b49957c21a19dcb19bae8d580e7
Zhangxingang <zhangxingang@bytedance.com>
2025-04-23 02:27:54 +0800

    add: rax3000q
    Signed-off-by: Zhangxingang <zhangxingang@bytedance.com>

--- target/linux/qualcommax/image/ipq50xx.mk
+++ target/linux/qualcommax/image/ipq50xx.mk
@@ -50,3 +50,20 @@ define Device/linksys_spnmx56
 		ipq-wifi-linksys_spnmx56
 endef
 TARGET_DEVICES += linksys_spnmx56
+
+define Device/cmcc_rax3000q
+   $(call Device/FitImage)
+   $(call Device/UbiFit)
+   SOC := ipq5000
+   DEVICE_VENDOR := CMCC
+   DEVICE_MODEL := RAX3000Q  DEVICE_MODEL := RAX3000Q
+   BLOCKSIZE := 128k
+   PAGESIZE := 2048
+   DEVICE_DTS_CONFIG := config@mp02.1
+   IMAGES := nand-factory.ubi
+   DEVICE_PACKAGES := \
+ 	ath11k-firmware-ipq5018 \
+ 	ath11k-firmware-qcn6122 \
+ 	ipq-wifi-cmcc_rax3000q	ipq-wifi-cmcc_rax3000q
+ endef
+ TARGET_DEVICES += cmcc_rax3000qTARGET_DEVICES += cmcc_rax3000q
